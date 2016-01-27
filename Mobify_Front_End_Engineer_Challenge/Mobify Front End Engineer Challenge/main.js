abd1a70709bd67d21edefdf8e266c44765177d54
Sebastien Luong <sebastienluong@gmail.com>
2016-01-27 02:03:03 -0800

    Clean up code

--- Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
+++ Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
@@ -30,14 +30,18 @@ var ID_TYPE = '#';
 var TAG_TYPE = 't';
 var UNIVERSAL = '*';
 
+/* definition of descendant selector taken from mdn:
+   https://developer.mozilla.org/en-US/docs/Web/CSS/Descendant_selectors
+*/
 var cssquery = window.cssquery = function(s) {
+
     // descendant queries take two selectors split 
     // by 1 or more space, new lines or tab
-    var trim_str = "   #bla    *   ".trim();  
+    var trim_str = "   #bla    a   ".trim();  
     var split_str = trim_str.split(" ");
     split_str = split_str.filter(remove_spaces);
 
-    // descendant query should take only 2 selectors
+    // descendant query should take 2 selectors
     if (split_str.length != 2){
         return [];
     }
@@ -53,37 +57,32 @@ var cssquery = window.cssquery = function(s) {
     if (ancestor_type == ID_TYPE){
        match_list = match_vals.children;
        for (var i=0; i<match_list.length; i++) {
-
           // push every value if universal selector is input
           if (descendant == UNIVERSAL){
              result_arr.push(match_list[i]);
           }
           else if (match_list[i].tagName == descendant.toUpperCase()){
-             console.log(match_list[i]);
              result_arr.push(match_list[i]);
           }
        }
     }
-    else{
+    else{   // Class type or Tag type
+
         // Get children from nodelist
         for (var i=0; i<match_vals.length; i++){
             match_list[i] = match_vals[i].children;
 
             for (var j=0; j<match_list[i].length; j++){
-
                 // '*' selector push all to result
                 if (descendant == UNIVERSAL){
                      result_arr.push(match_list[i]);
                 }
-
                 else if (match_list[i][j].tagName == descendant.toUpperCase()){
                     result_arr.push(match_list[i][j]); 
                 }   
             }
         }
-        
     }
-    console.log(result_arr);
     return result_arr;
 };
 
@@ -125,5 +124,5 @@ function query_by_type(ancestor, a_type) {
     }
 }
 
-// execute code on page load
-document.addEventListener("DOMContentLoaded", cssquery, false);
+// execute code on page load, for testing purposes
+//document.addEventListener("DOMContentLoaded", cssquery, false);
