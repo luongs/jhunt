a80f1f9897785d101ac48fe6f71d86c5ae5fcf18
Sebastien Luong <sebastienluong@gmail.com>
2016-01-27 00:44:31 -0800

    Get children with tags

--- Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
+++ Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
@@ -32,7 +32,7 @@ var TAG_TYPE = 't';
 var cssquery = window.cssquery = function(s) {
     // descendant queries take two selectors split 
     // by 1 or more space, new lines or tab
-    var trim_str = "   div     p  ".trim();  
+    var trim_str = "   .cool     a  ".trim();  
     var split_str = trim_str.split(" ");
     split_str = split_str.filter(remove_spaces);
 
@@ -47,12 +47,29 @@ var cssquery = window.cssquery = function(s) {
     var ancestor_type = match_ancestor_type(ancestor);
 
     var match_vals = query_by_type(ancestor, ancestor_type);
-    for (i=0; i<match_vals.length; i++){
-        console.log(match_vals.item(i))
+    var match_list = [];
+
+    if (ancestor_type == ID_TYPE){
+       console.log(match_vals.tagName);
+    }
+    else{
+        // Get children from nodelist
+        var children_list = []
+        for (var i=0; i<match_vals.length; i++){
+            match_list[i] = match_vals[i].children;
+            console.log(match_list[i]);
+        }
+        
+        // Target descendant tag type from children
+        for (var i=0; i< match_list.length; i++){
+            console.log(match_list[i][i]);
+            if (match_list[i].tagName == descendant)
+                console.log(match_list[i].tagName);
+        }
 
     }
 
-    document.getElementById("output").innerHTML = match_vals;
+    document.getElementById("output").innerHTML = match_vals[0];
     return [];
 };
 
@@ -79,9 +96,13 @@ function match_ancestor_type(ancestor) {
 function query_by_type(ancestor, a_type) {
     switch(a_type) {
         case CLASS_TYPE:
+            // remove '.'
+            ancestor = ancestor.substring(1);
             return document.getElementsByClassName(ancestor);
             break;
         case ID_TYPE:
+            // remove '#'
+            ancestor = ancestor.substring(1);
             return document.getElementById(ancestor);
             break;
         case TAG_TYPE:
