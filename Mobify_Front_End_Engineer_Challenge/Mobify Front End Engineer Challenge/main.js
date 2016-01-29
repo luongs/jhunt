f081637297225a1969ddbc0f9e4f4c7103c66361
Sebastien Luong <sebastienluong@gmail.com>
2016-01-29 08:55:24 -0800

    Make descendant array

--- Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
+++ Mobify_Front_End_Engineer_Challenge/Mobify Front End Engineer Challenge/main.js
@@ -37,31 +37,41 @@ var cssquery = window.cssquery = function(s) {
 
     // descendant queries take two selectors split 
     // by 1 or more space, new lines or tab
-    var trim_str = s.trim();  
+    var trim_str = "#bla p".trim();  
     var split_str = trim_str.split(" ");
     split_str = split_str.filter(remove_spaces);
 
-    // descendant query should take 2 selectors
-    if (split_str.length != 2){
+    // descendant query should take 2 or more selectors
+    if (split_str.length < 2){
         return [];
     }
      
     var ancestor = split_str[ANCESTOR];
-    var descendant = split_str[DESCENDANT];
+    var descendant = [];
     var ancestor_type = match_ancestor_type(ancestor);
 
     var match_vals = query_by_type(ancestor, ancestor_type);
     var match_list = [];
     var result_arr = [];
 
+    var amt_descendant = 0;
+    var match_count = 0;
+
+    for (var i=0; i<split_str.length-1; i++){
+        descendant[i] = split_str[i+1];
+        alert("Descendant "+descendant[i]);
+        amt_descendant++;
+    }
+
     if (ancestor_type == ID_TYPE){
        match_list = match_vals.children;
        for (var i=0; i<match_list.length; i++) {
           // push every value if universal selector is input
-          if (descendant == UNIVERSAL){
+          if (descendant[0] == UNIVERSAL){
              result_arr.push(match_list[i]);
           }
-          else if (match_list[i].tagName == descendant.toUpperCase()){
+          else if (match_list[i].tagName == descendant[0].toUpperCase()){
+             console.log(match_list[i]);
              result_arr.push(match_list[i]);
           }
        }
@@ -74,10 +84,10 @@ var cssquery = window.cssquery = function(s) {
 
             for (var j=0; j<match_list[i].length; j++){
                 // '*' selector push all to result
-                if (descendant == UNIVERSAL){
+                if (descendant[0] == UNIVERSAL){
                      result_arr.push(match_list[i]);
                 }
-                else if (match_list[i][j].tagName == descendant.toUpperCase()){
+                else if (match_list[i][j].tagName == descendant[0].toUpperCase()){
                     result_arr.push(match_list[i][j]); 
                 }   
             }
@@ -125,4 +135,4 @@ function query_by_type(ancestor, a_type) {
 }
 
 // execute code on page load, for testing purposes
-//document.addEventListener("DOMContentLoaded", cssquery, false);
+document.addEventListener("DOMContentLoaded", cssquery, false);
