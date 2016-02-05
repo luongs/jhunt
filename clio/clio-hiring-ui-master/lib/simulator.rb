596aab6501453b5ede9017b085169be69379ce12
Sebastien Luong <sebastienluong@gmail.com>
2016-02-05 00:55:27 -0800

    Complete Part 1

--- clio/clio-hiring-ui-master/lib/simulator.rb
+++ clio/clio-hiring-ui-master/lib/simulator.rb
@@ -71,82 +71,119 @@ class Simulator
     multi_array = @array_people
     len_row = multi_array.size
     len_col = multi_array[0].size
-    puts "START OF TEST"
-    res_hash = Hash[":hard"=>0,":soft"=>0,":none"=>0]
     res_char = ''
     current_char = ''
-    total_op = 0
+    total_h = 0
+    total_s = 0
     total_none = 0
+    total_op = 0
+    # Create new array to simulate simultaneous state transition
+    new_array = Array.new(len_row) {Array.new(len_col)}
 
     # Check values around array
     multi_array.each_with_index do |val,i|
       subarray = multi_array[i]
       subarray.each_with_index do |val2,j|
-        '''
+
         if (i > 0 and j > 0)
           res_char = multi_array[i-1][j-1]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (i> 0 and j< len_col-1)
           res_char = multi_array[i-1][j+1]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (i < len_row-1 and j > 0)
           res_char = multi_array[i+1][j-1]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (i<len_row-1 and j<len_col-1)
           res_char = multi_array[i+1][j+1]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (i>0)
           res_char = multi_array[i-1][j]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (i<len_row - 1)
           res_char = multi_array[i+1][j]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         if (j>0)
           res_char = multi_array[i][j-1]
-          res_hash[res_char]+=1
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
-        if (j<len_col)
-          res char = multi_array[i][j+1]
-          res_hash[res_char]+=1
+        if (j<len_col -1 )
+          res_char = multi_array[i][j+1]
+          case res_char
+            when :hard then total_h += 1
+            when :soft then total_s += 1
+            when :none then total_none += 1
+          end
         end
 
         current_char = multi_array[i][j]
-        total_h = res_hash[":hard"]
-        total_s = res_hash[":soft"]
         total_op = total_h+total_s
-        total_none = res_hash[":none"]
+
+        new_array[i][j] = current_char
 
         if (total_op<2)
-          multi_array[i][j] = ":none"
+          new_array[i][j] = :none
         elsif (total_op>3)
-          multi_array[i][j] = ":none"
-        elsif (total_op == 3)
+          new_array[i][j] = :none
+        elsif (total_op == 3 and current_char == :none)
           if (total_h>=2)
-            multi_array[i][j] = ":hard"
+            new_array[i][j] = :hard
           elsif (total_s>=2)
-            multi_array[i][j] = ":soft"
+            new_array[i][j] = :soft
           end
         end
-        '''
-        puts "Index i is #{i}, Index j is #{j}"
-        puts "Value: #{multi_array[i][j]}"
+
+        # reset totals
+        total_op = 0
+        total_s = 0
+        total_h = 0
+        total_none = 0
+
       end
     end   # end for while loops
-
-    return multi_array
+    @array_people = new_array
+    return @array_people
   end # end of next function
 
 end # end of class
