ee243d0337d614525084fe2f934b33bd82aa5bed
ma3lstrom <sebastienluong@gmail.com>
2016-02-16 09:32:54 -0800

    Add rx fp exercises

---
+++ essentials/eloquent_js/ch5/rx_tutorial/ex2_forEach.js
@@ -0,0 +1,6 @@
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+// forEach hides how the array is traversed
+names.forEach(function(name){
+  console.log(name);
+});
---
+++ essentials/eloquent_js/ch5/rx_tutorial/ex3_forEach_projection.js
@@ -0,0 +1,49 @@
+// Projection is the application of a function to value to create a new value.
+
+function project() {
+    var newReleases = [
+    {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id:432534, time:65876586 }]
+    },
+    {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id:432534, time:65876586 }]
+    }
+    ],
+  videoAndTitlePairs = [];
+  newReleases.forEach(function(movie){
+        var titleAndId = {};
+        titleAndId.id = movie.id;
+        titleAndId.title = movie.title;
+        videoAndTitlePairs.push(titleAndId);
+    });
+
+    return videoAndTitlePairs;
+}
+
+console.log(project());
