2a84a88cbd39a586e5924c9f7d7978f6184c76b0
ma3lstrom <sebastienluong@gmail.com>
2016-03-14 16:11:31 -0700

    Add functionality

---
+++ sauce_labs/magnificent/daemon.py
@@ -0,0 +1,60 @@
+import logging
+import requests
+import time
+
+LOG_FILE = 'magnificient.log'
+URL = 'http://localhost:12345'
+AMT_SLEEP = 2
+COUNT = 3
+STAT_MSG ="Success: {}, Fail: {}. Number of calls: {} in {} seconds \n"+\
+           "Total success: {}. Total fail: {} since running script"
+
+logging.basicConfig(filename=LOG_FILE,
+                    format='%(asctime)s %(message)s',
+                    level=logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+def getStats(success, fail, total_success, total_fail):
+    total_time = COUNT*AMT_SLEEP
+    stat_msg = STAT_MSG.format(success, fail, COUNT, total_time,
+                               total_success, total_fail)
+    return stat_msg
+
+def main():
+    total_success = 0
+    total_fail = 0
+    amt_success = 0
+    amt_fail = 0
+    counter = COUNT
+
+    while True:
+
+        if counter == 0:
+            logger.warning(getStats(amt_success, amt_fail, total_success,
+                                    total_fail))
+            counter = COUNT
+            amt_success = 0
+            amt_fail = 0
+
+        try:
+            r = requests.get(URL)
+            logger.warning(r.status_code)
+
+            if (r.status_code==200):
+                amt_success += 1
+                total_success += 1
+            else:
+                amt_fail += 1
+                total_fail += 1
+
+            counter -= 1
+            logger.warning(counter)
+            time.sleep(AMT_SLEEP)
+        except requests.exceptions.RequestException as e:
+            logger.warning(e)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
