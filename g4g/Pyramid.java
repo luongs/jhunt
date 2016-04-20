3c2868fb067f7d02c84738ff6d8512763bf0aed0
Sebastien Luong <sebastienluong@gmail.com>
2016-04-19 23:21:06 -0700

    Add $ pyramid

---
+++ g4g/Pyramid.java
@@ -0,0 +1,20 @@
+package g4g;
+
+public class Pyramid {
+	public static void printPyramid(int n){
+		for(int i=0;i<n;i++) {
+	        for(int j=0;j<n-i;j++) {
+	            System.out.print(" ");
+	        }
+	       for(int k=0;k<=i;k++) {
+	           System.out.print("$ ");
+	       }
+	       System.out.println();  
+	   }
+	}
+
+	public static void main(String[] args) {
+		printPyramid(5);
+	     
+	}
+}
